d3db5455106f6774557ce455a24264f0c5cb905a
Mokom Collins Awah <mokomcollinsawah@gmail.com>
2023-08-28 09:38:01 +0100

    first commit for my personal portfolio

---
+++ src/components/home/Socials.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Socials = () => {
+  return (
+    <div className='home__social'>
+        <a href="https://github.com/MokomCollinsAwah" className="home__social-icon" target='_blank'>
+            <i className="uil uil-github-alt"></i>
+        </a>
+        <a href="www.linkedin.com/in/mokomcollinsawah" className="home__social-icon" target='_blank'>
+            <i className="uil uil-linkedin"></i>
+        </a>
+        <a href="www.instagram.com/awahcollinsm" className="home__social-icon" target='_blank'>
+            <i className="uil uil-instagram"></i>
+        </a>
+        <a href="www.facebook.com/awahprincecollins" className="home__social-icon" target='_blank'>
+            <i className="uil uil-facebook"></i>
+        </a>
+        <a href="www.twitter.com/awahcollinsm" className="home__social-icon" target='_blank'>
+            <i className="uil uil-twitter"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Socials
